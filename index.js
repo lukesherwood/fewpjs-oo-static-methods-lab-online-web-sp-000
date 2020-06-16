fa036126c0885098c031bdb827f93db2b56ca8b8
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-16 22:15:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 class Formatter {
-  static capitalize(string){
+  static capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-  static sanitize(string){
+  static sanitize(string) {
     return string.replace(/[^A-Za-z0-9-']+/g, '');
   }
-  
+  static titleize(string) {
+    
+  }
   
 }
